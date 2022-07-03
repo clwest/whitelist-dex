ea2f62b860524ea07e04817dd15fe545d547654f
clwest <chris@donkeybetz.com>
2022-07-03 08:41:39 -0600

    reordered somethings

--- my-app/utils/swap.js
+++ my-app/utils/swap.js
@@ -1,9 +1,10 @@
 import { Contract } from "ethers";
 import {
-  EXCHANGE_CONTRACT_ABI,
   EXCHANGE_CONTRACT_ADDRESS,
-  TOKEN_CONTRACT_ABI,
+  EXCHANGE_CONTRACT_ABI,
   TOKEN_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  
 } from "../constants";
 
 export const getAmountOfTokensReceivedFromSwap = async (
