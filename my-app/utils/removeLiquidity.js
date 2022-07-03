68705263880131c055de251a73f75f4fbaeebe21
clwest <chris@donkeybetz.com>
2022-07-03 08:45:19 -0600

    missed a comma

--- my-app/utils/removeLiquidity.js
+++ my-app/utils/removeLiquidity.js
@@ -1,5 +1,5 @@
 import { Contract, providers, utils, BigNumber } from "ethers";
-import { EXCHANGE_CONTRACT_ABI, EXCHANGE_CONTRACT_ADDRESS } from "../constants";
+import { EXCHANGE_CONTRACT_ADDRESS, EXCHANGE_CONTRACT_ABI } from "../constants";
 
 export const removeLiquidity = async (signer, removeLPTokensWei) => {
     const exchangeContract = new Contract(
