30ee2d83a218b6e60b56dbccde5fadafee637de9
clwest <chris@donkeybetz.com>
2022-07-02 21:21:02 -0600

    l2JS

--- my-app/utils/removeLiquidity.js
+++ my-app/utils/removeLiquidity.js
@@ -3,8 +3,8 @@ import { EXCHANGE_CONTRACT_ABI, EXCHANGE_CONTRACT_ADDRESS } from "../constants";
 
 export const removeLiquidity = async (signer, removeLPTokensWei) => {
     const exchangeContract = new Contract(
-        EXCHANGE_CONTRACT_ABI,
         EXCHANGE_CONTRACT_ADDRESS,
+        EXCHANGE_CONTRACT_ABI,
         signer
     );
 
