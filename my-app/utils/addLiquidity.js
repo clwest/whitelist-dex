048c58ef8b306b2847aed7317f10ba34f84dbc53
clwest <chris@donkeybetz.com>
2022-07-03 08:39:08 -0600

    re up

--- my-app/utils/addLiquidity.js
+++ my-app/utils/addLiquidity.js
@@ -1,9 +1,10 @@
 import { Contract, utils } from "ethers";
 import {
-  EXCHANGE_CONTRACT_ABI,
   EXCHANGE_CONTRACT_ADDRESS,
-  TOKEN_CONTRACT_ABI,
+  EXCHANGE_CONTRACT_ABI,
   TOKEN_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  
 } from "../constants";
 
 
